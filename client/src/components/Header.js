68a2aef060c2823fcc05b41aed16047d7b310749
HarveyWang97 <461957024@qq.com>
2018-11-07 01:27:13 -0800

    router&redux&restructuring

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,37 +1,13 @@
-import React from 'react';
+import React,{Component} from 'react';
 import '../css/Header.css';
 import { Navbar} from 'reactstrap';
-import FacebookLogin from 'react-facebook-login';
-import axios from 'axios';
-//import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
+import LoginButton from './LoginButton';
 
-export default class Header extends React.Component{
+class Header extends Component{
     constructor(props){
         super(props);
-        this.state = {
-            isLoggedIn:false,
-            userID:'',
-            name:'',
-            email:'',
-        }
     }
 
-    responseFacebook = response => {
-        console.log(response);
-        this.setState({
-            isLoggedIn:true,
-            userId:response.userID,
-            name:response.name,
-            email:response.email
-        });
-        const url = 
-        `http://localhost:3000/users/insert?facebookId=${response.userID}&name=${response.name}&email=${response.email}`;
-        axios.get(url)
-        .then((response) => {
-            console.log(response);
-        })
-        
-    };
 
     render(){
         return (
@@ -40,18 +16,12 @@ export default class Header extends React.Component{
                     <span style={{fontSize:'22px'}}>Approaching</span>
                     <i className="fa fa-user-circle icon" ></i>
                 </Navbar>
-                <div className="login">
-                    <FacebookLogin
-                        appId="300879247180866"
-                        autoLoad
-                        fields="name,email,picture"
-                        callback={this.responseFacebook}
-                    />
-                </div>
             </div>
         );
     }
 }
+
+export default Header;
 
 
 
