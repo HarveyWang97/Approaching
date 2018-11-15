7c5b0aaabb8855f1221db0545376577899dcf675
Simeng Pang <pangsimeng@Simengs-MacBook-Pro.local>
2018-11-15 13:25:31 -0800

    event dashboard JSDoc

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -8,7 +8,7 @@ export default class Dashboard extends Component {
     constructor(props){
         super(props);
         this.state = {
-            showPopup: false
+            showPopup: true
         };
     }
 
