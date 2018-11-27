790eca2869d367697a124ca86d1daad2bd02ef98
Simeng Pang <pangsimeng@Simengs-MacBook-Pro.local>
2018-11-26 23:01:58 -0800

    fully finished timepicker

--- client/src/components/popup/Row.js
+++ client/src/components/popup/Row.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import '../../css/Popup.css';
 import Icon from './Icon';
+
+var today = new Date();
+var todayISO = today.toISOString().slice(0,16);
 /**
  * @classdesc Called by Popup to construct a pair of one Icon and one text value. 
  * 
@@ -47,10 +50,22 @@ class Row extends Component {
         console.log("time",mtime);
     }
 
+    timeConverter(UNIX_timestamp){
+        var a = new Date(UNIX_timestamp*1);
+        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        var year = a.getFullYear();
+        var month = months[a.getMonth()];
+        var date = a.getDate();
+        var hour = a.getHours() < 10 ? ('0'+a.getHours()) : a.getHours();
+        var min = a.getMinutes() < 10 ? ('0'+a.getMinutes()) : a.getMinutes();
+        var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min ;
+        return time;
+      }
+
     /**
 	 * Render the row based on the given input. 
      *
-     *  @param {String} field The data type of given text value, e.g. location, time.
+     * @param {String} field The data type of given text value, e.g. location, time.
      * @param {String} iconName The name of FAIcon to be constructed.
      * @param {Boolean} editing Whether the Popup is in editing mode.
      * @param {JsonObject} handleEditResult Pass handleEditResult behavior from Popup to this object.
@@ -58,28 +73,30 @@ class Row extends Component {
      * @return {html} Returns a html block of Popup component. 
 	 */
     render() {
-        const { iconName, details, editing } = this.props;        
+        const { iconName, details, editing } = this.props;
+        var showTime;
+        var dTime;
+        if (this.props.field === "time") { 
+            showTime = this.timeConverter(details); 
+            dTime = details ? new Date(details*1-28800000).toISOString().slice(0,16) : null; 
+        }
 
         return (
             <div className='popup_row'>
                 <Icon iconName={iconName}/>
-                {editing && iconName != "clock" ? (<input type="text" value={details} placeholder="Input"
+                {editing && this.props.field !== "time" ? (<input type="text" value={details} placeholder="Input"
                         onChange={this.handleChange.bind(this)} />)
-                        : (editing && iconName == "clock" ? null : (<span>{details}</span>))
+                        : (editing && this.props.field === "time" ? null : (this.props.field === "time" ? <span>{showTime}</span> : <span>{details}</span>))
                 }
                 {
-                    iconName == "clock" && editing ? 
-                    /*(<form  onSubmit={e => this.submitDate(e)}>
-                        <input id="datepicker" type="datetime-local" min="2018-11" max="2030-12"/>
-                        <input type="submit"/>
-                    </form>) : null*/
-                    (<input id="datepicker" type="datetime-local" min="2018-11" max="2030-12" onChange={this.handleChange.bind(this)}/>) : null
+                    this.props.field == "time" && editing ? 
+                    (<input id="datepicker" type="datetime-local" min={todayISO} max="2030-12-31T00:00" defaultValue={dTime} onChange={this.handleChange.bind(this)}/>) : null
                 }
                 {
-                    iconName == "list-ul" & editing ? (<button type="button" style={{marginLeft:'5px'}}>Add</button>) : null
+                    this.props.field === "itemList" & editing ? (<button type="button" style={{marginLeft:'5px'}}>Add</button>) : null
                 }
                 {
-                    iconName == "list-ul" & editing ? (<div style={{marginTop:'5px', marginLeft:'38px'}}><button type="button">Select From Item Board</button></div>) : null
+                    this.props.field === "itemList" & editing ? (<div style={{marginTop:'5px', marginLeft:'38px'}}><button type="button">Select From Item Board</button></div>) : null
                 }
             </div>
         );
