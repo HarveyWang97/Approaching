5d6f21639839640111f45adc354a0ac929610350
HarveyWang97 <461957024@qq.com>
2018-11-30 04:52:41 -0800

    setup actions test

--- client/src/__tests__/actions/toggle.test.js
+++ client/src/__tests__/actions/toggle.test.js
@@ -7,12 +7,25 @@ import {
     FETCH_ITEMS,
     TOGGLE_ITEM_SELECTOR,
     TOGGLE_EVENT_SELECTOR
-} from '../../types.js';
+} from '../../actions/types.js';
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import {getAction} from '../../utils/getAction';
 
-/*test('should picture editor toggle',() => {
-    const action = togglePopup({id:'123',contentType: 'event',isAdd: false, id: id});
-    expect(action).toEqual({
-        type:TOGGLE_POPUP,
-        payload:{id:'123'}
+export const mockStore = configureMockStore([thunk]);
+
+describe("test toggle functions",() => {
+    it("handles changing toggles", async () => {
+        const store = mockStore();
+        store.dispatch(togglePopup({id:'123',contentType:'event'}));
+        store.dispatch(togglePictureEditor({id:'123'}));
+        store.dispatch(toggleItemSelector({id:'dfs',formatted_details:[12,34]}))
+        expect(await getAction(store, TOGGLE_POPUP)).toEqual({type: TOGGLE_POPUP,payload:{id:'123',contentType:'event'}});
+        expect(await getAction(store, TOGGLE_PICTURE_EDITOR)).toEqual({type: TOGGLE_PICTURE_EDITOR,payload:{id:'123'}});
+        expect(await getAction(store, TOGGLE_ITEM_SELECTOR)).toEqual({type: TOGGLE_ITEM_SELECTOR,payload:{id:'dfs',formatted_details:[12,34]}});
     });
-});*/
+});
+
+
+
+
