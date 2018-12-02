5d576fc3b41bab70847b6ea147ba8475d751a09a
HarveyWang97 <461957024@qq.com>
2018-12-01 19:00:43 -0800

    test event

--- client/src/__tests__/actions/event.test.js
+++ client/src/__tests__/actions/event.test.js
@@ -8,7 +8,7 @@ import {getAction} from '../../utils/getAction';
 export const mockStore = configureMockStore([thunk]);
 
 describe("test fetch events",() => {
-    it("handle fetch and insert events",async() => {
+    it("handle fetch events",async() => {
         const store = mockStore();
        /* const data = {
             name:"Appfolio phone call",
@@ -18,13 +18,36 @@ describe("test fetch events",() => {
         };*/
         store.dispatch(fetchEvents('test','test'));
         const returnValue = await getAction(store,FETCH_EVENTS);
-        expect(returnValue.payload.rawEvents.length).toEqual(15);
+        //expect(returnValue.payload.rawEvents.length).toEqual(15);
         expect(returnValue.payload.rawEvents.filter(item => (item.name === "Appfolio phone call")).length).toEqual(1);
-       // expect(returnValue.payload.rawEvents[0].name).toEqual( "Appfolio phone call");
-      /*  expect(await getAction(store, FETCH_EVENTS).toEqual({
-            type:FETCH_EVENTS,
-
-        }));*/
-        
+        expect(returnValue.payload.rawEvents.filter(item => (item.name === "Facebook interview")).length).toEqual(1);
     });
 });
+
+describe("test insert events",() => {
+    it("handle  insert events",async() => {
+        const store = mockStore();
+        const data = {
+            name:"JWC basketball",
+            location:"John wooden center",
+            description:"xxx",
+            time:"15950000000"
+        };
+        await store.dispatch(insertEvent(data,'test','test'));
+        store.dispatch(fetchEvents('test','test'));
+        const returnValue = await getAction(store,FETCH_EVENTS);
+        const payload = returnValue.payload.rawEvents.filter(item => (item.name === "JWC basketball"));
+        console.log(payload[0]);
+        expect(payload[0].location).toEqual("John wooden center");
+       // expect(returnValue.payload.rawEvents.filter(item => (item.name === "JWC basketball"))[0].location)
+       // .toEqual("John wooden center");     
+    });
+});
+
+describe("describe delete events",() => {
+    it("handle delete events",async() => {
+        const store = mockStore();
+    });
+});
+
+
